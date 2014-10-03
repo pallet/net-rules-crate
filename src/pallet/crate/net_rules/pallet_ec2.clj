27726d2a08a436d3011d48e269dadcfaab09e20d
Hugo Duncan <hugo@hugoduncan.org>
2014-10-03 11:37:41 -0400

    Update pallet-ec2 backend to work with VPC
    Uses security group ids, rather than names, so that it works, in EC@
    classic, default VPC, and non-default VPC.

--- src/pallet/crate/net_rules/pallet_ec2.clj
+++ src/pallet/crate/net_rules/pallet_ec2.clj
@@ -22,19 +22,35 @@ namespace."
 (defn compute-service []
   (node/compute-service (:node (target))))
 
+(defn sg-id-for [compute vpc-id sg-name]
+  (let [filters [{:name "group-name" :values [sg-name]}]
+        filters (if vpc-id
+                  (conj filters {:name "vpc-id" :values [vpc-id]})
+                  filters)
+        {:keys [security-groups] :as sgs}
+        (pallet-ec2/execute
+         compute ec2/describe-security-groups-map {:filters filters})
+        matching-group (->> security-groups
+                            (filter #(and
+                                      (= vpc-id (:vpc-id %))
+                                      (= sg-name (:group-name %))))
+                            first)]
+    (debugf "sg-id-for %s %s %s" vpc-id sg-name (pr-str security-groups))
+    (:group-id matching-group)))
+
 (defn authorize
   "Authorize a cidr"
-  [compute sg-name port & {:keys [protocol ip-range region]}]
+  [compute sg-id port & {:keys [protocol ip-range region]}]
   (debugf "authorize %s %s %s" sg-name port ip-range)
   (try
     (pallet-ec2/execute
      compute
      ec2/authorize-security-group-ingress-map
-     {:group-name sg-name
-      :from-port port
-      :to-port port
-      :ip-protocol (name protocol)
-      :cidr-ip ip-range})
+     {:group-id sg-id
+      :ip-permissions [{:from-port port
+                        :to-port port
+                        :ip-protocol (name protocol)
+                        :ip-ranges [ip-range]}]})
     (catch Exception e
       (if (re-find #"has already been authorized" (.getMessage e))
         (debugf "Already authorized")
@@ -42,21 +58,22 @@ namespace."
 
 (defn authorize-targets
   "Authorize a port to a sequence of nodes"
-  [compute sg-name region {:keys [port protocol]} targets]
-  (debugf "authorize-targets %s %s %s" sg-name port (count targets))
+  [compute sg-id region {:keys [port protocol]} targets]
+  (debugf "authorize-targets %s %s %s" sg-id port (count targets))
   (doseq [target-map targets
           :let [ip-range (str (or (node/private-ip (:node target-map))
                                   (node/primary-ip (:node target-map)))
                               "/32")]]
     (try
-      (pallet-ec2/execute
-       compute
-       ec2/authorize-security-group-ingress-map
-       {:group-name sg-name
-        :from-port port
-        :to-port port
-        :ip-protocol (name protocol)
-        :cidr-ip ip-range})
+      (let [r (pallet-ec2/execute
+               compute
+               ec2/authorize-security-group-ingress-map
+               {:group-id sg-id
+                :ip-permissions [{:from-port port
+                                  :to-port port
+                                  :ip-protocol (name protocol)
+                                  :ip-ranges [ip-range]}]})]
+        (debugf "authorize-targets %s" r))
       (catch com.amazonaws.AmazonServiceException e
         (debugf e "Auth ingress failed")
         (throw e)))))
@@ -65,13 +82,17 @@ namespace."
   [_ permissions]
   (let [compute (compute-service)
         sg-name (sg-name (group-name))
-        region (target-region)]
-    (debugf "configure-net-rules %s" (pr-str permissions))
+        region (target-region)
+        vpc-id (:vpc-id (.info (:node (target))))
+        sg-id (sg-id-for compute vpc-id sg-name)]
+    (debugf "configure-net-rules %s vpc-id %s sg-name %s sg-id %s"
+            (pr-str permissions) vpc-id sg-name sg-id)
     (doseq [{:keys [cidr group role port protocol] :as permission} permissions]
       (cond
-       cidr (authorize compute sg-name port :protocol protocol
+       cidr (authorize compute sg-id
+                       port :protocol protocol
                        :ip-range cidr :region region)
-       group (authorize-targets compute sg-name region permission
+       group (authorize-targets compute sg-id region permission
                                 (targets-in-group group))
-       role (authorize-targets compute sg-name region permission
+       role (authorize-targets compute sg-id region permission
                                (targets-with-role role))))))
